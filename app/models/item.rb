948eb5cf0840ab8ac87fa569e983d6de6ae97a95
magic-onion <alexander.m.truong@gmail.com>
2018-10-24 10:35:43 -0400

    minor changes

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,5 +2,9 @@ class Item < ActiveRecord::Base
   has_many :cart_items
   has_many :carts, through: :cart_items
 
+  
 
 end
+
+#list all available items
+#add item to a cart
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ActiveRecord::Base
   has_many :carts
   has_many :items, through: :carts
 
+  
+
 end
 
 
