e46a9271d8d62e474b4df58f5dd525b70e634ba0
magic-onion <alexander.m.truong@gmail.com>
2018-10-24 12:37:38 -0400

    created cli.rb for creating user prompts

---
+++ bin/cli.rb
@@ -0,0 +1,67 @@
+#  user commands -
+#  cart history: all carts and prices
+# create new cart
+# add items
+# if invalid item, prompt for another item.
+#   read-tasks: averaging, total price, average price of carts
+
+#  user commands -
+#  cart history: all carts and prices
+# create new cart
+# add items
+# if invalid item, prompt for another item.
+#   read-tasks: averaging, total price, average price of carts
+# def welcome
+#   puts "Welcome to our Grocery!"
+# end
+
+
+#very basic outline-ish
+# def help
+#   puts "What would like to do today?"
+#   puts "- Type 'add'to add an item to your cart."
+#   puts "- Type 'update' to update a movie rating"
+#   puts "- Type 'history' to display your previous cart history"
+#   puts "- Type 'exit' to exit this program"
+# end
+
+# def login
+#   puts "Please enter your name:"
+#   name = gets.chomp
+#     if name user_id.nil?
+#       puts "Welcome, #{name}"
+#       #create new id
+#     else
+#       puts "Welcome back, #{name}"
+#     end
+#   help
+#   end
+# end
+
+# def display_cart
+
+# end
+
+# def exit
+#   puts "Goodbye!"
+#   exit
+# end
+
+
+# def run_list(cart)
+#   help
+#   input = ''
+#   while input
+#     puts "Please enter a command"
+#     input = gets.chomp
+#     case input
+#     when 'add' then add_items(cart)
+#     when 'update' then update_cart(cart)
+#     when 'history' then history(cart)
+#     when 'exit' then exit
+#       break
+#     else help
+#     end
+#   end
+#   run_list(cart)
+# end
