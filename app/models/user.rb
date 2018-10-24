ab1d3eb6ed3766497c6655b7dcabd67f1c76f678
magic-onion <alexander.m.truong@gmail.com>
2018-10-24 11:59:50 -0400

    user can add items to a cart

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,31 +18,27 @@ class User < ActiveRecord::Base
     puts "Your spend since the beginning of time is #{total}"
   end
 
-  # def add_item_to_cart(cart, item)
-  #   #makes sure the item exists
-  #   #makes sure the cart exists
-  #   #create a CartItem based on that
-  #   case
-  #   when !Item.find_by(name: name)
-  #     puts "that item does not seem to exist"
-  #   when Item.find_by(name: name)
-  #   end
-  # end
-
-
-
-
+  def add_item_to_cart(cart, item)
+    user_cart = Cart.find_by(name: cart)
+    user_item = Item.find_by(name: item)
+    if user_cart && user_item
+      user_cart.items << user_item
+    else
+      puts "That doesn't seem to be possible, please try again."
+    end
+  end
+    #would like to not require the user to enter their inputs in quotes
 
 end
 
-
-#User abilities:
-
+#
+# User abilities:
+#
 # create a cart
 # see all their carts < - u1.carts
 # see all the items in their carts
 # find out their total spend <- using a cart helper method.
-
+#
 #
 # u1.create_cart
 # => a new cart associated with that user
