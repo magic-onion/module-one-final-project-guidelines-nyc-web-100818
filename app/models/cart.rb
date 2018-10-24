873c0bdc26f3ba04d72a2e35b481772a49e6230a
magic-onion <alexander.m.truong@gmail.com>
2018-10-24 10:19:44 -0400

    wrote and did minor testing on cart methods

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -2,6 +2,26 @@ class Cart < ActiveRecord::Base
   has_many :cart_items
   has_many :items, through: :cart_items
 
+  def item_count
+    self.items.size
+  end
 
+
+  def cart_total
+    self.items.map {|item| item.price}.reduce(:+)
+  end
+
+  def price_per_item
+    total = self.cart_total
+    total/self.item_count
+  end
+
+  def list_items
+    self.items.map {|item| item.name}
+  end
 
 end
+
+# total the price of all the items inside
+# average price per item of the whole carts
+# show all items in the cart
