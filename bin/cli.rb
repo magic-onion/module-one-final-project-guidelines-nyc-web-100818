96713382c9fae76ede33e6141a7908dcca550bbf
magic-onion <alexander.m.truong@gmail.com>
2018-10-25 17:01:59 -0400

    method organization within cli.rb

--- bin/cli.rb
+++ bin/cli.rb
@@ -35,7 +35,6 @@ class Cli
     puts "Thank you for using Grocery Tracker. Please enter your username"
   end
 
-  #user must select a cart to do anything
   def select_cart
     puts "please enter a cart name to view or create one"
     cart_data = gets.chomp
@@ -66,6 +65,11 @@ class Cli
     @user.carts.each {|cart| puts "#{cart.name}", ""}
   end
 
+  def exit
+    puts "Goodbye!"
+    exit!
+  end
+
   def cart_total
     select_cart_prompt
     total = @cart.cart_total
@@ -75,6 +79,8 @@ class Cli
     puts "The total price of this cart is $#{total}0"
     end
   end
+
+  #cart options methods
 
   def list_items_and_prices
     select_cart_prompt
@@ -89,18 +95,6 @@ class Cli
     avg = @cart.avg_price_per_item
     puts "The average price per item of this cart is $#{avg.round(2)}"
   end
-
-  def cart_options_helper
-    puts "What would like to do today?", ""
-    puts "- Type 'go back' to visit previous menu", ""
-    puts "- Type 'items and prices' to display cart contents with prices", ""
-    puts "- Type 'price per item' to display this cart's average price per item", ""
-    puts "- Type total' to display this cart's total cost", ""
-    puts "- Type 'help' to display cart options", ""
-    puts "- Type 'exit' to exit"
-  end
-
-
 
   def item_spend
     puts "please enter an item"
@@ -117,7 +111,58 @@ class Cli
     puts "Your carts cost an average of $#{result}0", ""
   end
 
+  #main menu
+  def help
+    puts "What would like to do today?", ""
+    puts "-Type 'select cart' to start checking groceries", ""
+    puts "- Type 'add' to add an item to your cart.", ""
+    puts "- Type 'list' to list all items available for purchase", ""
+    puts "- Type 'view' to view the contents of your cart", ""
+    puts "- Type 'total' to display your current cart total", ""
+    puts "- Type 'history' to display all of your carts", ""
+    puts "- Type 'history options' to enter history menu", ""
+    puts "- Type 'cart options' to enter cart menu and view cart options", ""
+    puts "- Type 'exit' to exit this program", ""
+  end
+
+  def run_list
+    help
+    input = ''
+    while input
+      puts "Please enter a command", ""
+      input = gets.chomp
+      case input
+      when 'select cart' then select_cart
+      when 'add' then add_item
+      when 'list' then list_items
+      when 'view' then display_cart
+      when 'history' then history
+      when 'total'then cart_total
+      when 'cart options' then cart_options
+      when 'history options' then history_options
+      when 'exit' then exit
+        break
+      else puts "sorry, I'm not sure what that means"
+        help
+      end
+    end
+    run_list
+  end
+
+  #cart options menu
+  def cart_options_helper
+    puts "What would like to do today?", ""
+    puts "- Type 'go back' to visit previous menu", ""
+    puts "- Type 'items and prices' to display cart contents with prices", ""
+    puts "- Type 'price per item' to display this cart's average price per item", ""
+    puts "- Type total' to display this cart's total cost", ""
+    puts "- Type 'help' to display cart options", ""
+    puts "- Type 'exit' to exit"
+  end
+
   def cart_options
+    #most expensive item
+    #remove item?
     select_cart_prompt
     cart_options_helper
     input = ''
@@ -130,19 +175,17 @@ class Cli
       when 'price per item' then avg_price_per_item
       when 'total' then cart_total
       when 'view' then display_cart
-
       when 'help' then cart_options_helper
       when 'exit' then exit
         break
       else puts "sorry, I'm not sure what that means"
-         cart_options_helper
+        cart_options_helper
       end
     end
     cart_options
   end
 
-  #most expensive item
-  #remove item?
+  #history options menu
   def history_options_helper
     puts "What would like to do today?", ""
     puts "- Type 'go back' to visit previous menu", ""
@@ -168,52 +211,11 @@ class Cli
       when 'exit' then exit
         break
       else puts "sorry, I'm not sure what that means"
-         history_options_helper
+        history_options_helper
       end
     end
     history_options
   end
 
-def help
-  puts "What would like to do today?", ""
-  puts "-Type 'select cart' to start checking groceries", ""
-  puts "- Type 'add' to add an item to your cart.", ""
-  puts "- Type 'list' to list all items available for purchase", ""
-  puts "- Type 'view' to view the contents of your cart", ""
-  puts "- Type 'total' to display your current cart total", ""
-  puts "- Type 'history' to display all of your carts", ""
-  puts "- Type 'history options' to enter history menu", ""
-  puts "- Type 'cart options' to enter cart menu and view cart options", ""
-  puts "- Type 'exit' to exit this program", ""
-end
-
-  def run_list
-    help
-    input = ''
-    while input
-      puts "Please enter a command", ""
-      input = gets.chomp
-      case input
-      when 'select cart' then select_cart
-      when 'add' then add_item
-      when 'list' then list_items
-      when 'view' then display_cart
-      when 'history' then history
-      when 'total'then cart_total
-      when 'cart options' then cart_options
-      when 'history options' then history_options
-      when 'exit' then exit
-        break
-      else puts "sorry, I'm not sure what that means"
-         help
-      end
-    end
-    run_list
-  end
-
-  def exit
-   puts "Goodbye!"
-   exit!
- end
 
 end
