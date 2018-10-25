b2ec8d86997f6541b9501af18e45a60903452a44
magic-onion <alexander.m.truong@gmail.com>
2018-10-25 13:43:04 -0400

    some cleanup

--- bin/run.rb
+++ bin/run.rb
@@ -2,9 +2,6 @@ require_relative '../config/environment'
 require_relative './cli.rb'
 require 'pry'
 
-
-puts "Welcome to a Grocery Checker"
-
 a = Cli.new("thing")
 a.welcome_prompt
 username = gets.chomp
